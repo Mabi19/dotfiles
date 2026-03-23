eee5c0ae0d7eb47a1dfa38107ed652bae321476f
Mabi19 <mateuszbartnik33@gmail.com>
2026-03-23 19:39:32 +0100

    Update screenshot script

--- dot_config/hypr/scripts/executable_screenshot.sh
+++ dot_config/hypr/scripts/executable_screenshot.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-coproc spaceshot defer
+# 
+
+coproc spaceshot defer output
 read -r READY <&"${COPROC[0]}"
 
 if [ "$READY" != "ready" ]; then
     exit 1
 fi
 
+# Window mode isn't done, so don't include it for now
 CHOICE=$(printf "Region\\nOutput" | walker -d)
 
 case "$CHOICE" in
     "Region") ARGS=("region") ;;
-    "Output") ARGS=("output") ;;
+    "Monitor") ARGS=("output") ;;
+    "Window") ARGS=("cancel") ;;
     *) exit 1 ;;
 esac
 
@@ -21,5 +25,10 @@ done
 
 # shellcheck disable=SC2093,SC1083
 exec {COPROC[1]}>&-
-
 wait "$COPROC_PID"
+
+if [ $CHOICE = "Window" ] then
+    # figure out how to dmenu this.
+    # A walker lua script might work better here.
+    # But the toplevel picker would just be the best option.
+fi
